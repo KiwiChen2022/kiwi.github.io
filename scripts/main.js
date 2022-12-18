fd1d802552e446f826ef6823e8a7abd7b6d168b1
KiwiChen2022 <120965995+KiwiChen2022@users.noreply.github.com>
2022-12-19 01:34:42 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/google_icon_512x512.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+  }
+  
+/*
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+  localStorage.setItem('name', null);
+/*
+if (!localStorage.getItem('name')){
+    setUserName();
+} else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+*/
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+
+myButton.onclick = function(){
+    setUserName();
+  }
